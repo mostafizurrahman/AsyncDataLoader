b53c4a64ebfab85adc96b3c772167d417c26fa48
Mostafizur Rahman <contacts.tumblr@gmail.com>
2019-06-17 17:38:16 +0600

    Project added.

---
+++ AsyncDataLoader/AsyncDataLoader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AsyncDataLoader
+//
+//  Created by NoboPay on 17/6/19.
+//  Copyright © 2019 Mostafizur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
